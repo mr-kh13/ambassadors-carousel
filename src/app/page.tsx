09c04f335931d82d522fcf4fcae629ce264c61e3
Mohammad <Mohammad.kh06@gmail.com>
2025-01-27 19:18:05 +1100

    Implement carousel page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,48 @@
+"use client";
+import { AmbassadorCard } from "@/components/AmbassadorCard";
 import styles from "./page.module.scss";
+import { ArrowButtons, Carousel, CarouselApi } from "@/components/Carousel";
+import { Slide } from "@/components/Carousel/Slide";
+import { AMBASSADORS } from "@/data/ambassadors";
+import { useState } from "react";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+  const [carouselApi, setCarouselApi] = useState<CarouselApi | null>(null);
+
+  return (
+    <div className={styles.page}>
+      <Carousel
+        onSlideChange={setActiveSlide}
+        loop
+        classes={{ container: styles.carouselContainer }}
+        getCarouselApi={setCarouselApi}
+      >
+        {AMBASSADORS.map((ambassador, index) => {
+          return (
+            <Slide key={ambassador.name}>
+              <AmbassadorCard
+                image={ambassador.image}
+                signature={ambassador.signature}
+                background={ambassador.background}
+                title={ambassador.tagLine}
+                name={ambassador.name}
+                quote={ambassador.quote}
+                active={activeSlide === index}
+              />
+            </Slide>
+          );
+        })}
+      </Carousel>
+      <div className={styles.arrowButtonsWrapper}>
+        {carouselApi && (
+          <ArrowButtons
+            carouselApi={carouselApi}
+            classes={{ prev: styles.prevButton, next: styles.nextButton }}
+          />
+        )}
+      </div>
+      <div className={styles.background} />
+    </div>
+  );
 }
---
+++ src/data/ambassadors.ts
@@ -0,0 +1,56 @@
+interface Ambassador {
+  name: string;
+  image: string;
+  tagLine: string;
+  quote: string;
+  background: string;
+  signature: string;
+}
+
+export const AMBASSADORS: Array<Ambassador> = [
+  {
+    name: "TERRE OWENS",
+    tagLine: "MEMBER OF THE PRO FOOTBALL HALL OF FAME",
+    quote:
+      "Pushing limits is what I do. This brand gets that—together, we're redefining what’s possible.",
+    background: "/carousel/terrel-owens-background.png",
+    signature: "/carousel/terrel-owens-signature.png",
+    image: "/carousel/terrel-owens-image.png",
+  },
+  {
+    name: "AQIB TALIB",
+    tagLine: "QUARTER BACK FOR THE DENVER BRONCOS",
+    quote:
+      "They’re not just about the game—they’re about the passion, the grind, and the love for it. That’s why this collaboration means so much.",
+    background: "/carousel/aqib-talib-background.png",
+    signature: "/carousel/aqib-talib-signature.png",
+    image: "/carousel/aqib-talib-image.png",
+  },
+  {
+    name: "FLOYD MAYWEATHER",
+    tagLine: "The best defensive boxer of all time",
+    quote:
+      "Greatness respects greatness. That’s why I’m teaming up with this brand, giving you a chance to be great.",
+    background: "/carousel/floyd-mayweather-background.png",
+    signature: "/carousel/floyd-mayweather-signature.png",
+    image: "/carousel/floyd-mayweather-image.png",
+  },
+  {
+    name: "MARSHALL FAULK",
+    tagLine: "MEMBER OF THE PRO FOOTBALL HALL OF FAME",
+    quote:
+      "Every step, every shot, every move matters. This brand believes in that same relentless focus, and that’s why I’m here.",
+    background: "/carousel/marshall-faulk-background.png",
+    signature: "/carousel/marshall-faulk-signature.png",
+    image: "/carousel/marshall-faulk-image.png",
+  },
+  {
+    name: "DANNY KENELL",
+    tagLine: "QUARTER BACK FOR THE DENVER BRONCOS",
+    quote:
+      "For me, it’s about inspiring the next generation. This brand shares that mission, and together, we’re making it happen.",
+    background: "/carousel/danny-kenell-background.png",
+    signature: "/carousel/danny-kenell-signature.png",
+    image: "/carousel/danny-kenell-image.png",
+  },
+];
